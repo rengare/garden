1740419233654818102a512dd7764b60d073c38c
ggorecki <gorecki.gracjan@gmail.com>
2024-02-13 18:57:23 +0100

    changes

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,7 +1,6 @@
 use assert_fs::prelude::*;
-use miette::ErrReport;
 use predicates::prelude::*;
-use rexpect::{error::Error, session::spawn_command};
+use rexpect::session::spawn_command;
 use std::process::Command;
 
 #[test]
@@ -23,38 +22,4 @@ fn test_write_help() {
         .assert()
         .success()
         .stderr("");
-}
-
-#[test]
-fn test_write_with_title() -> Result<(), Box<dyn Error>> {
-    let temp_dir = assert_fs::TempDir::new()?;
-
-    let bin_path = assert_cmd::cargo::cargo_bin("garden");
-    let fake_editor_path = std::env::current_dir()?
-        .join("tests")
-        .join("fake-editor.sh");
-    if !fake_editor_path.exists() {
-        panic!("fake editor shell script could not be found")
-    }
-
-    let mut cmd = Command::new(bin_path);
-    cmd.env("EDITOR", fake_editor_path.into_os_string())
-        .env("GARDEN_PATH", temp_dir.path())
-        .arg("write")
-        .arg("-t")
-        .arg("atitle");
-
-    let mut process = spawn_command(cmd, Some(30000))?;
-
-    process.exp_string("current title: ")?;
-    process.exp_string("atitle")?;
-    process.exp_regex("\\s*")?;
-    process.exp_string("Do you want a different title? (y/N): ")?;
-    process.send_line("N")?;
-    process.exp_eof()?;
-
-    temp_dir
-        .child("atitle.md")
-        .assert(predicate::path::exists());
-    Ok(())
 }
